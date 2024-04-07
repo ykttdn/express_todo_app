d2d92dd2ad38d2ce412a57b8c4fd0d356b4ffecc
ykttdn <38461803+ykttdn@users.noreply.github.com>
2024-04-07 17:58:48 +0900

    Add GET method for getting all todos

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,16 @@ app.get("/", (_, res) => {
   res.send("Hello world");
 });
 
+app.get("/todos", (_, res) => {
+  const todos = [
+    { id: 1, name: "buy eggs" },
+    { id: 2, name: "clean the kitchen" },
+    { id: 3, name: "wash the clothes" },
+  ];
+
+  res.json(todos);
+});
+
 app.listen(PORT, () => {
   console.log(`Server ready at: http://localhost:${PORT}`);
 });
